49eb79d26254d0d2739f023fecc0c1ed2c626913
Admin <Admin@DESKTOP-6OFRCDD>
2019-11-25 18:43:58 +0530

    Potential Module Create Feature

---
+++ src/main/java/com/CRM/Pages/CreatePotential.java
@@ -0,0 +1,121 @@
+package com.crm.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CreatePotential 
+{
+	@FindBy(xpath="//a[.='Potentials']") private WebElement potMod;
+	public WebElement getPotMod() {
+		return potMod;
+	}
+	public void clickPotMod() {
+		potMod.click();
+	}
+	
+	@FindBy(xpath="//input[@value='New Potential']") private WebElement newPotBtn;
+	public WebElement getNewPotBtn() {
+		return newPotBtn;
+	}
+	public void clickNewPotBtn() {
+		newPotBtn.click();
+	}
+	
+	@FindBy(name="property(Potential Name)") private WebElement potNmTb;
+	public WebElement getPotNmTb() {
+		return potNmTb;
+	}
+	public void setPotNmTb(String pn) {
+		potNmTb.sendKeys(pn);
+	}
+	
+	@FindBy(xpath="//img[@title='Account Name Lookup']") private WebElement accLook;
+	public WebElement getAccLook() {
+		return accLook;
+	}
+	public void clickAccLook() {
+		accLook.click();
+	}
+	
+	@FindBy(xpath="//a[.='Next']") private WebElement nextBtn;
+	public WebElement getNextBtn() {
+		return nextBtn;
+	}
+	public void clickNextBtn() {
+		nextBtn.click();
+	}
+	
+	@FindBy(xpath="//a[@class='link']") private List<WebElement> accList;
+	public List<WebElement> getAccList() {
+		return accList;
+	}
+	public void clickAccList() {
+		for(WebElement oneAcc:accList)
+		{
+			if(oneAcc.getText().equals("Jack"))
+			{
+				oneAcc.click();
+				break;
+			}
+		}
+	}
+	
+	@FindBy(xpath="//img[@title='Calendar']") private WebElement clsDate;
+	public WebElement getClsDate() {
+		return clsDate;
+	}
+	public void clickClsDate() {
+		clsDate.click();
+	}
+	
+	@FindBy(name="property(Stage)") private WebElement stageDD;
+	public WebElement getStageDD() {
+		return stageDD;
+	}
+	public void clickStageTb() {
+		stageDD.click();
+	}
+	
+	@FindBy(name="property(Type)") private WebElement typeDD;
+	public WebElement getTypeDD() {
+		return typeDD;
+	}
+	public void clickTypeDD() {
+		typeDD.click();
+	}
+	
+	@FindBy(name="property(Amount)") private WebElement amtTB;
+	public WebElement getAmtTB() {
+		return amtTB;
+	}
+	public void setAmtTB() {
+		amtTB.sendKeys("25000");
+	}
+	
+	@FindBy(name="property(Lead Source)") private WebElement leadDD;
+	public WebElement getLeadDD() {
+		return leadDD;
+	}
+	public void clickLeadDD() {
+		leadDD.click();
+	}
+	
+	@FindBy(xpath="(//input[@name='Button'])[1]") private WebElement saveBtn;
+	public WebElement getSaveBtn()
+	{
+		return saveBtn;
+	}
+	public void clickSaveBtn()
+	{
+		saveBtn.click();
+	}
+	
+	public CreatePotential(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+}
---
+++ src/test/java/com/CRM/Login/VerifyCRMLogin.java
@@ -0,0 +1,77 @@
+package com.crm.loginLib;
+
+import java.util.Set;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.crm.genericlibrary.BaseTest;
+import com.crm.genericlibrary.FileLibrary;
+import com.crm.genericlibrary.WebDriverCommonLibrary;
+import com.crm.pages.CRMHomePage;
+import com.crm.pages.CRMLoginPage;
+import com.crm.pages.CreatePotential;
+
+public class VerifyCRMLogin extends BaseTest
+{
+	WebDriver driver;
+	CRMHomePage chp;
+	CRMLoginPage clp;
+	FileLibrary flib;
+	WebDriverCommonLibrary wlib;
+	CreatePotential cp;
+	
+	@Test
+	public void crmLogin() throws Throwable 
+	{
+		chp=new CRMHomePage(driver);
+		clp=new CRMLoginPage(driver);
+		flib=new FileLibrary();
+		wlib=new WebDriverCommonLibrary();
+		cp=new CreatePotential(driver);
+		
+		Reporter.log("Home page is displayed --- passed",true);
+		cp.clickPotMod();
+		cp.clickNewPotBtn();
+		cp.setPotNmTb(flib.getCellData(EXCEL_PATH, "Sheet1", 1, 0));
+		cp.clickAccLook();
+		Set<String> allwhs=driver.getWindowHandles();
+		for(String wh:allwhs)
+		{
+			String title=driver.switchTo().window(wh).getTitle();
+			if(title.contains("Account Name"))
+			{
+				cp.clickAccList();
+				break;
+			}
+			else
+			{
+				cp.clickNextBtn();
+				cp.clickAccList();
+				break;
+			}
+		}
+		cp.clickClsDate();
+		Set<String> allwhs1=driver.getWindowHandles();
+		for(String wh:allwhs1)
+		{
+			String title=driver.switchTo().window(wh).getTitle();
+			if(title.contains("Closing Date"))
+			{
+				JavascriptExecutor jse=(JavascriptExecutor)driver;
+				jse.executeScript("document.getElementByTagName('a')[29].click()");
+				break;
+			}
+		}
+		cp.clickStageTb();
+		wlib.selectOption(cp.getStageDD(), 5);
+		cp.clickTypeDD();
+		wlib.selectOption(cp.getTypeDD(), 1);
+		cp.setAmtTB();
+		cp.clickLeadDD();
+		wlib.selectOption(cp.getLeadDD(), 1);
+		cp.clickSaveBtn();
+	}
+}
