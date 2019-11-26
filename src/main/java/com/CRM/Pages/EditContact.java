da80f6670f63f230eb331c3d41e8fb056d5a9445
Rohan <Ganesh Malaji@LAPTOP-OIM4O9E4>
2019-11-26 21:14:55 +0530

    Contacts

---
+++ src/main/java/com/CRM/Pages/EditContact.java
@@ -0,0 +1,70 @@
+package com.CRM.Pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.CRM.genericLib.FileLib;
+import com.CRM.genericLib.IAutoConsts;
+
+
+public class EditContact implements IAutoConsts 
+{
+	@FindBy(xpath="(//a[@class='menuOff'])[3]") private WebElement contMod;
+	public WebElement getContMod() 
+	{
+		return contMod;
+	}
+	public void clickContMod() 
+	{
+		contMod.click();
+	}
+	
+	@FindBy(xpath="(//input[@name='edit2'])[1]") private WebElement editBtn;
+	public WebElement getEditBtn() 
+	{
+		return editBtn;
+	}
+	public void clickEditBtn() 
+	{
+		editBtn.click();
+	}
+
+	@FindBy(xpath="(//input[@name='Button'])[1]") private WebElement savebtn;
+	public WebElement getSavebtn() 
+	{
+		return savebtn;
+	}
+	public void clickSavebtn() 
+	{
+		savebtn.click();
+	}
+	
+	@FindBy(xpath="//table[2]//td[3]//a") private List<WebElement> contList;
+	public List<WebElement> getContList() 
+	{
+		return contList;
+	}
+	public void clickContList() throws Throwable 
+	{
+		for(WebElement oneCont:contList)
+		{
+			FileLib flib=new FileLib();
+			if(oneCont.getText().equalsIgnoreCase(flib.getCellData(EXCEL_PATH, "Invalid", 1, 6)))
+			{
+				oneCont.click();
+				clickEditBtn();
+				clickSavebtn();
+				break;
+			}
+		}
+	}
+	
+	public EditContact(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+}
